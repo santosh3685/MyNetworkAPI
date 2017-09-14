ca3b5b2ff4e85a8805cb943de25ebbe63b767830
santosh3685 <31529616+santosh3685@users.noreply.github.com>
2017-09-14 12:54:50 +0530

    Project added

---
+++ MyNetworkAPI/MyNetworkAPI/MyNetworkAPI.h
@@ -0,0 +1,19 @@
+//
+//  MyNetworkAPI.h
+//  MyNetworkAPI
+//
+//  Created by santosh on 14/09/17.
+//  Copyright © 2017 TechM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyNetworkAPI.
+FOUNDATION_EXPORT double MyNetworkAPIVersionNumber;
+
+//! Project version string for MyNetworkAPI.
+FOUNDATION_EXPORT const unsigned char MyNetworkAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNetworkAPI/PublicHeader.h>
+
+
